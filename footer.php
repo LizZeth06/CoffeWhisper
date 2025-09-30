a9fa6d9f8d549722cd30494ded491d3a365af035
LizZ <mabylrm11@gmail.com>
2025-09-30 15:20:04 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+  <div class="footer-content">
+    <div class="footer-logo">Coffee <span class="shimmer">Whisper</span></div>
+    <p class="footer-text">Hecho con ☕ y amor </p>
+    <p class="footer-copy">&copy; <?= date('Y') ?> Coffee Whisper by JSL. Todos los derechos reservados.</p>
+  </div>
+</footer>
